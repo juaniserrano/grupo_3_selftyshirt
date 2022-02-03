882e6bbca3d6501121de9a70af739e4f8b1864ce
juaniserrano <juanignacioserranoluna@gmail.com>
2022-02-03 07:23:12 -0300

    se modifico y se agregn html y css

--- app.js
+++ app.js
@@ -1,20 +1,23 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-
-app.listen(3000, ()=>{
-    console.log('Servidor funcionando');
+app.listen(3000, () => {
+  console.log("Servidor funcionando");
 });
 
-app.get('/', (req,res)=>{
-    res.sendFile(__dirname + '/views/index.html');
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
 });
 
-app.get('/login', (req,res)=>{
-    res.sendFile(__dirname + '/views/login.html');
+app.get("/login", (req, res) => {
+  res.sendFile(__dirname + "/views/login.html");
 });
 
-app.get('/register', (req,res)=>{
-    res.sendFile(__dirname + '/views/register.html');
+app.get("/register", (req, res) => {
+  res.sendFile(__dirname + "/views/register.html");
+});
+
+app.get("/cart", (req, res) => {
+  res.sendFile(__dirname + "/views/productCart.html");
 });
