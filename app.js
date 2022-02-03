f3a8282fd9cbc904ca104bd6415e82fd6eadffaf
juaniserrano <juanignacioserranoluna@gmail.com>
2022-02-02 21:47:38 -0300

    se agrega register html y css

--- app.js
+++ app.js
@@ -14,3 +14,7 @@ app.get('/', (req,res)=>{
 app.get('/login', (req,res)=>{
     res.sendFile(__dirname + '/views/login.html');
 });
+
+app.get('/register', (req,res)=>{
+    res.sendFile(__dirname + '/views/register.html');
+});
