cc120d9f6e5c53a37dca3d8c1cc4c6c8c5aa9ead
Evitrompiz <evitrompiz12@gmail.com>
2022-04-24 19:00:00 -0300

    se agrega sequelize

---
+++ src/database/config/config.js
@@ -0,0 +1,24 @@
+module.exports = 
+{
+  "development":{
+    "username": "root",
+    "password": null,
+    "database": "structure",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "test": {
+    "username": "root",
+    "password": null,
+    "database": "structure",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "production": {
+    "username": "root",
+    "password": null,
+    "database": "structure",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  }
+}
